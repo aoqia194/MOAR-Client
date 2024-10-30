08c302dbffb4bc9c63e9f724a0ba51585dc719c2
andrewgdewar <andrewgdewar@gmail.com>
2024-10-30 17:22:41 -0600

    Working first version

---
+++ Patches/MoarPatch.cs
@@ -0,0 +1,29 @@
+﻿using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace MOAR.ExamplePatches
+{
+    internal class MoarPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GameWorld), nameof(GameWorld.OnGameStarted));
+        }
+
+        [PatchPrefix]
+        static bool Prefix()
+        {
+            var preset = Plugin.GetServerString();
+            Plugin.PostMessage(preset);
+            return true;
+        }
+
+        [PatchPostfix]
+        static void Postfix()
+        {
+            // code in Postfix() method runs AFTER the original code is executed
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,34 @@
-﻿namespace MOAR
-{
-    public class Plugin
-    {
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MOAR.ExamplePatches;
+using ChatShared;
+using EFT.Communications;
+using EFT;
 
+namespace MOAR
+{
+    [BepInPlugin("MOAR.UniqueGUID", "MOAR", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+        public static string GetServerString()
+        {
+            var req = SPT.Common.Http.RequestHandler.GetJson("/moar/currentPreset");
+            return req.ToString(); // no need to parse bare strings
+        }
+        public static void PostMessage(string message)
+        {
+            NotificationManagerClass.DisplayMessageNotification(message, ENotificationDurationType.Long);
+        }
+        // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake()
+        {
+            new MoarPatch().Enable();
+        }
     }
 }
